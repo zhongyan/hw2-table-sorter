e6c287fa723f07630a9b340f9db8a42836418f10
zero-mo <502442134@qq.com>
2015-03-14 15:22:14 +0800

    13331202-莫冠钊

--- 13331202_莫冠钊/sorter.js
+++ 13331202_莫冠钊/sorter.js
@@ -13,7 +13,7 @@ function makeAllTablesSortable(tables) {
 		ths = tables[i].getElementsByTagName("th");
 		trs = tables[i].getElementsByTagName("tr");
 		for (j = 0; j < ths.length; j++) {
-			/*用Attribute来完成传参数的目的,因为在循环里事件传的参数会按照变量最后出现的值来传*/
+			/*用Attribute来完成传参数的目的,因为在循环里click事件传的参数会按照循环里变量最后出现的值来传*/
 			ths[j].setAttribute("table_num", i);
 			ths[j].setAttribute("col", j);
 			ths[j].setAttribute("trs_num", trs.length-1);
@@ -61,13 +61,38 @@ function sort_table(table_num, col, is_ascend, trs_num) {
 	var i, j, temp;
 	for (i = 1; i <= trs_num; i++) {
 		for (j = 1; j <= trs_num-1; j++) {
-			if ((is_ascend&&table.rows[j].cells[col].innerHTML > table.rows[j+1].cells[col].innerHTML)||
-				(!is_ascend&&table.rows[j].cells[col].innerHTML < table.rows[j+1].cells[col].innerHTML)) {
-				temp = table.rows[j].innerHTML;
-				table.rows[j].innerHTML = table.rows[j+1].innerHTML;
-				table.rows[j+1].innerHTML = temp;
+			if (judge(table.rows[j].cells[col], table.rows[j+1].cells[col], is_ascend)) {
+				swap(table.rows[j], table.rows[j+1]);
 			}
 		}
 	}
 }
 
+/*判断是否需要改变位置*/
+function judge(a, b, is_ascend) {
+	/*先把该标签的HTML空格去除，以便下面正则表达式检查数字(sicily上的HTML某些数字左右是有空格的)*/
+	var a_rm_white = a.innerHTML.replace(/(^\s+)|(\s+$)/g,"").replace(/\s/g,"");
+	var b_rm_white = b.innerHTML.replace(/(^\s+)|(\s+$)/g,"").replace(/\s/g,"");
+	/*用正则表达式来判断是否为数字(包括正负整数或浮点数)*/
+	var re = /^(-?\d+)(\.\d+)?$/;
+	if (re.test(a_rm_white)&&re.test(b_rm_white)) {
+		if (is_ascend) {
+			return parseFloat(a.innerHTML) > parseFloat(b.innerHTML);
+		} else {
+			return parseFloat(a.innerHTML) < parseFloat(b.innerHTML);
+		}
+	} else {
+		if (is_ascend) {
+			return a.innerHTML > b.innerHTML;
+		} else {
+			return a.innerHTML < b.innerHTML;
+		}
+	}
+}
+
+/*交换位置*/
+function swap(a, b) {
+	var temp = a.innerHTML;
+	a.innerHTML = b.innerHTML;
+	b.innerHTML = temp;
+}
