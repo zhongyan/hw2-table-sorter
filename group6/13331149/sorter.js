8b530869bbf57fb8f2b3e45bfd99256f758dd63a
linjinjin <494779656@qq.com>
2015-03-14 23:56:06 +0800

    Update sorter.js

--- group6/13331149/sorter.js
+++ group6/13331149/sorter.js
@@ -1,59 +1,98 @@
 window.onload = function() {
-	makeAllTablesSortable();
+	var tables = getAllTables();
+	makeAllTablesSortable(tables);
 }
 
-function makeAllTablesSortable() {
-	var tables = document.getElementsByTagName("table");
-	for (var i = 0; i < tables.length; i++) {
-		//table = tables[i];
-		for (var j = 1; j < tables[i].rows.length; j++) {
-			//改变偶数行颜色
-			if (j%2 == 0) {
-				tables[i].rows[j].style.backgroundColor = "#BDBDBD"; 
-			}
+//get All tables in the page
+function getAllTables() {
+	return tables = document.getElementsByTagName("table");
+}
+
+//change the color in even rows
+function ChangeRowsColor(table) {
+	for (var j = 1; j < table.rows.length; j++) {
+		if (j%2 == 0) {
+			table.rows[j].style.backgroundColor = "#BDBDBD"; 
 		}
+	}
+}
+
+//sort the table by iCol
+function SortTable(table, iCol, rows) {
+	var datas = new Array;
+	for (var i = 0; i < rows.length; i++) {
+		datas[i] = rows[i];
+	}
+	if (table.sortCol == iCol) {
+		datas.reverse();
+	} else {
+	 	datas.sort(compEle(iCol));
+	}
+	return datas;
+}
+
+//compare two element
+function compEle(iCol) {
+	 return function(a, b) {
+	 	a.cells[iCol].innerHTML > b.cells[iCol].innerHTML;
+	 }
+}
+
+//show the sorted table
+function ShowNewTable(table, datas, rows) {
+	var oFragment = document.createDocumentFragment();
+	for (var k = 0; k < datas.length; k++) {
+		datas[k].style.backgroundColor = "white";
+		oFragment.appendChild(datas[k]);
+	}
+	table.tBodies[0].appendChild(oFragment);
+	for (var i = 0; i < rows.length; i++) {
+		if (i%2 == 1) {
+			rows[i].style.backgroundColor = "#BDBDBD"; 
+		}
+	}
+}
+
+//change the th stley.
+function ChangeThStyle(th) {
+	if (th.lastChild.className == "ascend-icon")
+		th.lastChild.className = "descend-icon";
+	else
+		th.lastChild.className = "ascend-icon"; 
+}
+
+function createIco(th) {
+	var para = document.createElement("span");
+	th.appendChild(para);
+}
+
+function makeAllTablesSortable(tables) {
+	for (var i = 0; i < tables.length; i++) {
+		ChangeRowsColor(tables[i]);
+		
 		cols = tables[i].rows[0].cells.length;
 		for (var j = 0; j < cols; j++) {
 			th = tables[i].rows[0].cells[j];
-			var para = document.createElement("span");
-			th.appendChild(para);
+			createIco(th)
+			//listen:sort table when click th
 			th.onclick = function() {
-				table = this.parentNode.parentNode.parentNode;
-				var datas = new Array;
-				var rows = table.tBodies[0].rows;
+				var table = this.parentNode.parentNode.parentNode;
 				var iCol = this.cellIndex;
-				for (var i = 0; i < rows.length; i++) {
-					datas[i] = rows[i];
-				}
-				if (table.sortCol == iCol) {
-					datas.reverse();
-				} else {
-				 	datas.sort(function(a, b) {
-				 		return a.cells[iCol].innerHTML > b.cells[iCol].innerHTML;
-				 	});
-				}
-				if (this.lastChild.className == "ascend-icon")
-						this.lastChild.className = "descend-icon";
-					else
-						this.lastChild.className = "ascend-icon"; 
-				var oFragment = document.createDocumentFragment();
-				for (var k = 0; k < datas.length; k++) {
-					datas[k].style.backgroundColor = "white";
-					oFragment.appendChild(datas[k]);
-				}
-				table.tBodies[0].appendChild(oFragment);
+				var datas;
+				var rows = table.tBodies[0].rows;
+
+				ChangeThStyle(this);
+				datas = SortTable(table, iCol, rows);
+				ShowNewTable(table, datas, rows);
 				table.sortCol = iCol;
-				for (var i = 0; i < rows.length; i++) {
-					if (i%2 == 1) {
-						rows[i].style.backgroundColor = "#BDBDBD"; 
-					}
-				}
 			};
+			//init table style
 			th.onmouseover = function() {
 				this.style.backgroundColor = "#CBEAEC";
 				this.style.cursor = "pointer";
 				this.childNodes[1].className = "ascend-icon";
 			};
+			//init table style
 			th.onmouseout = function() {
 				this.style.backgroundColor = "#120562";
 			}
