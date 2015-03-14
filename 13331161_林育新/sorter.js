7963a3d72c4216c4d0dc717e524b88d3e2085d07
Yacent <540608491@qq.com>
2015-03-14 12:29:20 +0800

    some change for js

--- 13331161_林育新/sorter.js
+++ 13331161_林育新/sorter.js
@@ -21,19 +21,25 @@ function makeAllTablesSortable(tables) {
 function tableSort() {
     var tmpForRows = [];
     var trColIndex = this.cellIndex;
+    var tmpRows = [];
     //此为获得table下除了thead以外的所有行
-    var trRows = this.parentNode.parentNode.parentNode.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
+    var trRows = this.parentNode.parentNode.parentNode.getElementsByTagName("tr");
     var thsSibiling = this.parentNode.getElementsByTagName("th");
 
-    for (var i = 0; i < trRows.length; i++) {  //将表格每一行的内容存入一个二维数组
+    for (var i = 1; i < trRows.length; i++) {
+        tmpRows[i-1] = trRows[i];
+    }
+
+    for (var i = 0; i < tmpRows.length; i++) {  //将表格每一行的内容存入一个二维数组
         tmpForRows[i] = [];
-        for (var j = 0; j < trRows[i].cells.length; j++) { // 每一行有多少列
-            tmpForRows[i][j] = trRows[i].cells[j].innerHTML;
+        for (var j = 0; j < tmpRows[i].cells.length; j++) { // 每一行有多少列
+            tmpForRows[i][j] = tmpRows[i].cells[j].innerHTML;
         }
     }
 
     tmpForRows.sort(function (a, b) {
-        return a[trColIndex] > b[trColIndex];
+        if (isNaN(a[trColIndex])) return a[trColIndex] > b[trColIndex];
+        else return parseInt(a[trColIndex]) > parseInt(b[trColIndex]);
     })
 
     for (var i = 0; i < thsSibiling.length; i++) {
@@ -50,8 +56,8 @@ function tableSort() {
         tmpForRows.reverse();
     }
 
-    for (var i = 0; i < trRows.length; i++) {
-        trRows[i].innerHTML = "<td>" + tmpForRows[i].join("</td><td>") + "</td>";
+    for (var i = 0; i < tmpRows.length; i++) {
+        tmpRows[i].innerHTML = "<td>" + tmpForRows[i].join("</td><td>") + "</td>";
     }
 }
 
